f97bbe8d3c01858dd4e246dbb53bbcf37f636fcd
Zrain-X <43879272+Zrain-X@users.noreply.github.com>
2026-09-08 10:13:36 +0800

    chore(pages): trigger deployment for UI fixes

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,6 +8,7 @@ import HomeHero from '../../components/HomeHero.vue'
 import CategoryIndex from '../../components/CategoryIndex.vue'
 import ModelExplorer from '../../components/ModelExplorer.vue'
 import ModelCompare from '../../components/ModelCompare.vue'
+// Keep final visual corrections last so they override theme and component defaults.
 import './layout-fixes.css'
 
 export default {
